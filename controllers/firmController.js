058e6722fde8105f6fcdb26fc32fdbf2aa8b1d68
G Nagasrinivas <nagasrinivas4488@gmail.com>
2024-12-09 21:13:33 +0530

    first commit

---
+++ controllers/firmController.js
@@ -0,0 +1,60 @@
+const Firm = require('../models/Firm');
+const Vendor =require('../models/Vendor');
+const multer =require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'uploads/'); 
+    },
+    filename: (req, file, cb) => {
+    
+      cb(null, Date.now() + '+', path.extname(file.originalname));
+    }
+  });
+  
+  const upload =multer ({storage:storage});
+
+
+const addFirm = async(req, res)=>{
+    try {
+        const {firmName,area,category,region,offer} =req.body
+
+const iamge =req.file? req.file.filename:undefined;
+
+
+
+const vendor = await Vendor.findById(req.vendorId)
+if(!vendor){
+    res.status(404).json({message:"vendor not found"})
+}
+
+const firm=new Firm({
+    firmName,area,category,region,offer,vendor:vendor._id
+})
+ const savedFirm = await firm.save();
+
+ vendor.firm.push(savedFirm)
+ await vendor.save()
+
+return res.status(200).json({message:'Firm Added successfully'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json("internal server error")
+        
+    }
+
+}
+const deleteFirmById= async(req,res)=>{
+  try {
+      const firmId =req.params.firmId;
+      const deletedproduct = await firm.findByIdAndDelete(firmId);
+      if(!deletedproduct){
+          return res.status.json({error:"No product found"})
+      }
+      
+  } catch (error) {
+      console.log(error)
+      res.status(500).json({error:"Internal server error"})   
+  }
+}
+module.exports={addFirm:[upload.any('image'),addFirm],deleteFirmById}
---
+++ controllers/vendorControllers.js
@@ -0,0 +1,95 @@
+
+const jwt =require('jsonwebtoken')
+const bcrypt =require('bcryptjs');
+const Vendor = require('../models/Vendor');
+const dotEnv =require('dotenv');
+
+dotEnv.config();
+
+const secretKey =process.env.WhatIsYourName
+
+const vendorRegister = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+
+        const vendorEmail = await Vendor.findOne({ email }); 
+        if (vendorEmail) {
+            return res.status(400).json({ message: "Email already taken" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newVendor = new Vendor({
+            username,
+            email,
+            password: hashedPassword, 
+        });
+
+        await newVendor.save();
+
+        res.status(201).json({ message: "Vendor registered successfully" });
+        console.log("registered ");
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+const vendorLogin = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if vendor exists
+        const vendor = await Vendor.findOne({ email });
+        if (!vendor) {
+            return res.status(401).json({ error: "Invalid username or password" });
+        }
+
+        // Compare passwords
+        const isPasswordValid = await bcrypt.compare(password, vendor.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid username or password" });
+        }
+        const token =jwt.sign({ vendorId: vendor._id},secretKey,{expiresIn:"1h"})
+        // Login successful
+        res.status(200).json({ success: "Login successful",token});
+        console.log(email,"this is token")
+        
+      
+    } catch (error) {
+      console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+const getAllVendors = async (req, res) =>{
+    try {
+        
+        const vendors =await Vendor.find()
+        return res.json(vendors)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+const getVendorById =async(req,res)=>{
+    const vendorId=req.params.id;
+    try {
+       const Vendor = await vendor.findById(vendorId)
+       if(!vendor){
+        return res.status(404).json({error:"vendor not found"})
+       }
+       return res.status(200).json({vendor})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+
+    }
+
+}
+
+
+module.exports={vendorRegister ,vendorLogin, getAllVendors,getVendorById }

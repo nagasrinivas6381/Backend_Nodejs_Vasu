5de3b4999d46ffbadcd826407f8c76ca79b1834a
G Nagasrinivas <nagasrinivas4488@gmail.com>
2024-12-10 10:41:13 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const app = express()
 
-const PORT= 4000;
+const PORT= process.env.PORT || 4000;
 dotEnv.config()
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB connected successfully"))
@@ -27,6 +27,6 @@ app.listen(PORT,()=>{
     console.log(`server started and running at ${PORT}`); 
 })
 
-app.use('/home',(req,res)=>{
+app.use('/',(req,res)=>{
     res.send("<h1>Welocome To Vasu");
 })

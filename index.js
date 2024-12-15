ede5973ec1c5cdf38e3b97b488050cae6ce66c76
G Nagasrinivas <nagasrinivas4488@gmail.com>
2024-12-15 17:40:33 +0530

    adding one item

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.use('/uploads',express.static('uploads'));
 app.listen(PORT,()=>{
     console.log(`server started and running at ${PORT}`); 
 })
+app.use(express.static(path.join(__dirname, "./dist")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
 
 app.use('/',(req,res)=>{
     res.send("<h1>Welocome To Vasu");
